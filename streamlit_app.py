f06ab37f3ab7280eafd1f8d183a20d93ad41d782
tirumalaravikanth <137619423+tirumalaravikanth@users.noreply.github.com>
2023-06-24 08:56:04 -0400

    Update streamlit_app.py
    added menu and details

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,5 +2,7 @@ import streamlit
 
 streamlit.title('My parents new Healthy Diner')
 
-streamlit.header('breakfast menu')
-streamlit.text('This is my first webpage')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
